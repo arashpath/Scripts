a2bcee28e4c68fffc963365fb74c87a36b766464
arashpath <arashpath@gmail.com>
2017-06-22 17:58:35 +0530

    Fully Working and Tested
    added db user and password change

--- Linux/fssai/magnolia.sh
+++ Linux/fssai/magnolia.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#Script to Change DB string in Magnolia
-Magnolia="/opt/APPS/default-tom0/webapps/ROOT/"
-#DB Server:	OldSvr="localhost" ; NewSvr="postgresql"
-Svr_o="psqldb01" ; Svr_n="psqldb01"
-#DB Name:	OldDB="fssai-demo" ; NewDB="fssaimain"
-DB_o="fssaimain" ; DB_n="fssaimain"
-#DBUser
-for i in $(grep -R jdbc:postgresql://$Svr_o:5432/$DB_o "$Magnolia" -l)
-	do sed -i "/jdbc:postgresql:\/\/$Svr_o:5432\/$DB_o/ 	{
-		s/$Svr_o/$Svr_o/
-		s/$DB_o/$DB_o/	}" $i
+#Script to Change Pos DB string in Magnolia
+Magnolia="/opt/APPS/mainHI-tom6/webapps/ROOT/"
+DB_Svr="psqldbsvr01"
+DB_Port="5432"
+DB_Name="magdb"
+DB_User="magdbuser"
+DB_Pass="magdbpass"
+db_url="jdbc:postgresql://$DB_Svr:$DB_Port/$DB_Name"
+
+for i in $(grep -R '<param name="url" value="jdbc:postgresql:' "$Magnolia" -l)
+        do sed -i "/<param name=\"url\" value=\"jdbc:postgresql:/s!value=\".*\"!value=\"$db_url\"!" $i
+           sed -i "/<param name=\"user\"/s/value=\".*\"/value=\"$DB_User\"/" $i
+           sed -i "/<param name=\"password\"/s/value=\".*\"/value=\"$DB_Pass\"/" $i
 done
