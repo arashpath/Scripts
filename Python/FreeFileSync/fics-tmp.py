acd6c4c68e00526f482534cfbcba57ba423eba1f
Prashaath R <arashpath@gmail.com>
2017-06-23 14:30:58 +0530

    Modified Script for Folder Wise sync

--- Python/FreeFileSync/fics-tmp.py
+++ Python/FreeFileSync/fics-tmp.py
@@ -1,30 +1,49 @@
+#!/usr/bin/python
 from datetime   import date
 from calendar   import monthrange
 from string     import Template
 from datetime   import datetime
 from subprocess import call
+from os         import remove
 def add_months(sourcedate,months):
     month = sourcedate.month - 1 + months
-    year = int(sourcedate.year + month / 12 )
+    year  = int(sourcedate.year + month / 12 )
     month = month % 12 + 1
-    day = min(sourcedate.day,monthrange(year,month)[1])
+    day   = min(sourcedate.day,monthrange(year,month)[1])
     return date(year,month,day)
 	
 bkp_month = date(2015,1,1)
-present = date.today()
+present   = date.today()
+
+folders = ['MISDocs','UserImage','LabDocs','AppDocs']
 
 while bkp_month.strftime("%y%m") < present.strftime("%y%m") :
-    print "Year: %s , Month: %s" % ( bkp_month.strftime("%Y"), bkp_month.strftime("%b") )
-    filein = open( 'fics-tmp.templ' )
-    src = Template( filein.read() )
     year = bkp_month.strftime("%Y")
     month = bkp_month.strftime("%b")
-    d={ 'year':year, 'month':month }
-    result = src.substitute(d)
-    ffsbatch = open("fics-tmp.ffs_batch", 'w')
-    ffsbatch.write(result)
-    ffsbatch.close()
-    call(['C:\\Program Files\\FreeFileSync\\FreeFileSync.exe', 'D:\\Scripts\\Temp\\fics-tmp.ffs_batch'])
-    bkp_month = add_months(bkp_month,1)
+    
+    print "\nSyncing %s, %s." % ( month, year )
+    #raw_input("Press Enter to continue...")
+    for folder in folders:
+        print "  %s..." % (folder),
+        filein = open( 'fics-tmp.templ' )
+        src = Template( filein.read() )
+        d={ 'year':year, 'month':month, 'folder':folder }
+        result = src.substitute(d)
+        batch_file = year+month+"_"+folder+".ffs_batch"
+        ffsbatch = open( batch_file, 'w')
+        ffsbatch.write(result)
+        ffsbatch.close()
+        #raw_input("Press Enter to continue...")
+        ret = call(['C:\\Program Files\\FreeFileSync\\FreeFileSync.exe', batch_file])
+        if ret == 0:
+            print "\t Done"
+        elif ret == 1:
+            print "\t Done with Warnings!"
+        elif ret == 2:
+            print "\t Done with Errors!!"
+        elif ret == 3:
+            print "\t Aborted!!!"
+        bkp_month = add_months(bkp_month,1)
+        remove(batch_file)
 
 
