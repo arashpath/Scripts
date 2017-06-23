4ff817aadf23372d580d79e11426e62d0acaade2
Prashaath R <arashpath@gmail.com>
2017-06-23 15:14:58 +0530

    Working!!

--- Python/FreeFileSync/fics-tmp.py
+++ Python/FreeFileSync/fics-tmp.py
@@ -5,6 +5,7 @@ from string     import Template
 from datetime   import datetime
 from subprocess import call
 from os         import remove
+
 def add_months(sourcedate,months):
     month = sourcedate.month - 1 + months
     year  = int(sourcedate.year + month / 12 )
@@ -12,8 +13,9 @@ def add_months(sourcedate,months):
     day   = min(sourcedate.day,monthrange(year,month)[1])
     return date(year,month,day)
 	
-bkp_month = date(2015,1,1)
+bkp_month = date(2015,2,1)
 present   = date.today()
+##present   = date(2015,1,1)
 
 folders = ['MISDocs','UserImage','LabDocs','AppDocs']
 
@@ -21,20 +23,23 @@ while bkp_month.strftime("%y%m") < present.strftime("%y%m") :
     year = bkp_month.strftime("%Y")
     month = bkp_month.strftime("%b")
     
-    print "\nSyncing %s, %s." % ( month, year )
-    #raw_input("Press Enter to continue...")
+    print "\nSyncing....  %s, %s" % ( month, year )
+    ##raw_input("Press Enter to continue...")
     for folder in folders:
-        print "  %s..." % (folder),
+        print "  %s" % (folder),
         filein = open( 'fics-tmp.templ' )
         src = Template( filein.read() )
         d={ 'year':year, 'month':month, 'folder':folder }
         result = src.substitute(d)
+        
         batch_file = year+month+"_"+folder+".ffs_batch"
         ffsbatch = open( batch_file, 'w')
         ffsbatch.write(result)
         ffsbatch.close()
-        #raw_input("Press Enter to continue...")
+        
+        ##raw_input("Press Enter to continue...")
         ret = call(['C:\\Program Files\\FreeFileSync\\FreeFileSync.exe', batch_file])
+        ##ret = 0
         if ret == 0:
             print "\t Done"
         elif ret == 1:
@@ -43,7 +48,8 @@ while bkp_month.strftime("%y%m") < present.strftime("%y%m") :
             print "\t Done with Errors!!"
         elif ret == 3:
             print "\t Aborted!!!"
-        bkp_month = add_months(bkp_month,1)
-        remove(batch_file)
+            
+    bkp_month = add_months(bkp_month,1)
+    remove(batch_file)
 
 
