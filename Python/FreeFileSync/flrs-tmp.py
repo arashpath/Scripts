97851780f8e233683f3ab9c371bd9eb1988493a7
Prashaath R <arashpath@gmail.com>
2017-07-15 10:10:54 +0530

    Modified scripts for all year and all months

--- Python/FreeFileSync/flrs-tmp.py
+++ Python/FreeFileSync/flrs-tmp.py
@@ -1,42 +1,47 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 from string     import Template
 from subprocess import call
 from os         import remove
+from time       import strftime
 
-months  = ['6','5','4','3','2','1']
-folders = [ ['Fdrive/FSSAI-DOCS1/FLRS','CLS'],
-            ['Edrive/FSSAI-DOCS/FLRS','SLS'],
-            ['Fdrive/FSSAI-DOCS1/FLRS','REG']  ]
+year = ['2016','2015','2017']
 
-for m in months:
-    print "\nSyncing 2017"
+folders = [
+    ['Fdrive/FSSAI-DOCS1/FLRS','CLS'],
+    ['Edrive/FSSAI-DOCS/FLRS','SLS'],
+    ['Fdrive/FSSAI-DOCS1/FLRS','REG']
+    ]
+
+for y in year:
+    for m in range(12,0,-1):
+        print "\nSyncing %s %s" %(y, m)
     
-    for f in folders:
-        drive  = f[0]
-        folder = f[1]
-        print " %s\t" %(folder),
+        for f in folders:
+            drive  = f[0]
+            folder = f[1]
+            print "%s %s" %(strftime("%H:%M:%S %d/%m/%Y"),folder),
 
-        templ = open( 'flrs-tmp.tmpl' )
-        src = Template( templ.read() )
-        d={ 'drive':drive, 'folder':folder, 'm':m }
-        result = src.substitute(d)
+            templ = open( 'flrs-tmp.tmpl' )
+            src = Template( templ.read() )
+            d={ 'drive':drive, 'folder':folder, 'y':y, 'm':m }
+            result = src.substitute(d)
         
-        batch_file = "flrs-"+folder+"_"+m+".ffs_batch"
-        ffsbatch = open( batch_file, 'w')
-        ffsbatch.write(result)
-        ffsbatch.close()
+            batch_file = "flrs"+folder+"_"+str(y)+str(m)+".ffs_batch"
+            ffsbatch = open( batch_file, 'w')
+            ffsbatch.write(result)
+            ffsbatch.close()
 
-        raw_input("Press Enter to continue...")
-        ret = call(['C:\\Program Files\\FreeFileSync\\FreeFileSync.exe', batch_file])
-        ##ret = 0
-        if ret == 0:
-            print "\t Done"
-        elif ret == 1:
-            print "\t Done with Warnings!"
-        elif ret == 2:
-            print "\t Done with Errors!!"
-        elif ret == 3:
-            print "\t Aborted!!!"  
-        remove(batch_file)
+            ret = call(['C:\\Program Files\\FreeFileSync\\FreeFileSync.exe', batch_file])
+            ##ret = 0       #for Testing if above line is commented
+            if ret == 0:
+                print "\t Done"
+            elif ret == 1:
+                raw_input("\t Done with Warnings! Continue..?\n")
+            elif ret == 2:
+                raw_input("\t Done with Errors!!  Continue..?\n")
+            elif ret == 3:
+                raw_input("\t Aborted!!!          Continue..?\n")
+            
+            remove(batch_file)
 
 
