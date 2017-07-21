e249d3ce0fdfb08d34ff88bf8dd597451e241d16
Prashaath R <arashpath@gmail.com>
2017-07-21 17:25:51 +0530

    STG 1: Calculating total size

--- Python/PURGING/FLRS_purging.py
+++ Python/PURGING/FLRS_purging.py
@@ -7,28 +7,42 @@ mssql = pyodbc.connect('DRIVER={SQL Server};SERVER=192.168.11.201;DATABASE=FLRS;
 
 
 arcLoc = 'G:\PURGED_DOCS\FLRS'
-### arcLoc = 'D:\FSSAI_DOCS\DSC'
-purgeFiles = (
+
+purgeFiles = [
     ['CLS', 'F:\FSSAI-DOCS1\FLRS'],
     ['SLS', 'E:\FSSAI-DOCS\FLRS'],
     ['REG', 'F:\FSSAI-DOCS1\FLRS']   
-)
- 
+] 
 
+all_sum = 0
 for p in purgeFiles:
     (doc_type, base_path) = p
+    base_path = 'D:\FSSAI-DOCS\FLRS' #OverRide Actual base path for local testing
     sql_query = "SELECT [refid], [PATH], [issueddate], [ExpireDate] FROM FLRS.dbo.EXPLICDATA WHERE [TYPE] = '"+doc_type+"' AND [PATH] is NOT NULL ORDER BY [ExpireDate], [refid]"
     mssql.execute(sql_query)
     rows = mssql.fetchall()
-    sum = 0 
-    for row in rows:
-        arcFile = os.path.normpath(row.PATH)
-        srcPath = os.path.join(base_path, doc_type, arcFile) 
-        arcPath = os.path.join(arcLoc, doc_type, arcFile)
-        print ("Moving from: "+srcPath+"\n\t to: "+arcPath)
-        if os.path.isfile(srcPath):
-            f = os.path.getsize(srcPath)
-            #os.renames(srcPath,arcPath)
-            print ("Archived: %s "+arcFile)
-            sum = sum + f
-    print (hb(sum))
+
+    with open(doc_type+'_purging.log','w') as log_file:
+        doc_sum = 0
+        for row in rows:
+            arcFile = os.path.normpath(row.PATH)
+            srcPath = os.path.join(base_path, doc_type, arcFile) 
+            arcPath = os.path.join(arcLoc, doc_type, arcFile)
+            #print ("Moving from: "+srcPath+"\n\t to: "+arcPath)
+            if os.path.isfile(srcPath):
+                f = os.path.getsize(srcPath)
+                #os.renames(srcPath,arcPath)
+                #print ("Archived: %s "+arcFile)
+                doc_sum = doc_sum + f
+                print (hb(doc_sum)+" Found\t: "+srcPath, file=log_file)
+            else:
+                print ("File Not Found\t: "+srcPath, file=log_file)
+
+        print (30*"=", file=log_file)
+        print ("Grand Total of "+doc_type+" = "+hb(doc_sum), file=log_file)
+        print (30*"=", file=log_file)
+
+    all_sum = all_sum + doc_sum
+    input (hb(doc_sum)+" cleared in "+doc_type+"Enter to continue...")
+
+print ("Total Space Saved After Purging : "+hb(all_sum))
