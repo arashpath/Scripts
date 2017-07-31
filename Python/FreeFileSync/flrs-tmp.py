861c43097507be49d8695db6deca04e828e0e124
Prashaath R <arashpath@gmail.com>
2017-07-31 14:12:09 +0530

    Added support for base Folder

--- Python/FreeFileSync/flrs-tmp.py
+++ Python/FreeFileSync/flrs-tmp.py
@@ -3,49 +3,63 @@ from string     import Template
 from subprocess import call
 from os         import remove
 from time       import strftime
+from sys        import argv
 
+# Function To Create a ffs_batch and run FreeFileSync
+def run_ffs(folders,y='',m='',exclude=''):
+    """ A function to Run FFS"""
+    for f in folders.keys():
+        print "%s %s" %(strftime("%H:%M:%S %d/%m/%Y"),f),
+
+        templ = open( 'flrs-tmp.tmpl' )
+        src = Template( templ.read() )
+        d={ 'drive':folders.get(f), 'folder':f, 'y':y, 'm':m, 'exclude':exclude }
+        result = src.substitute(d)
+
+        batch_file = "flrs"+f+str(y)+str(m)+".ffs_batch"
+        with open( batch_file, 'w') as ffsbatch:
+            ffsbatch.write(result)
+
+        #raw_input("Press Enter to continue...")
+        ret, tr = 1, 1
+        while not ret == 0 and tr <= 3:
+                ret = call(['C:\\Program Files\\FreeFileSync\\FreeFileSync.exe', batch_file])
+                tr += 1 
+        if ret == 0:
+            print "=>> Done"
+        elif ret == 1:
+            print "=>> Done with Warnings!" , raw_input("\t Continue..?\n")
+        elif ret == 2:
+            print "=>> Done with Errors!!"  , raw_input("\t Continue..?\n")
+        elif ret == 3:
+            print "=>> Aborted!!!      "    , raw_input("\t Continue..?\n")
+
+        remove(batch_file)
+
+
+# Define Folders and their Base path
+folders = {
+    'CLS':'Fdrive/FSSAI-DOCS1/FLRS',
+    'SLS':'Edrive/FSSAI-DOCS/FLRS', 
+    'REG':'Fdrive/FSSAI-DOCS1/FLRS'
+    }
+
+# Def Years
 year = ['2015','2016','2017']
-#year = ['2016']
 
-folders = [
-    ['Fdrive/FSSAI-DOCS1/FLRS','CLS'],
-    ['Edrive/FSSAI-DOCS/FLRS','SLS'],
-    ['Fdrive/FSSAI-DOCS1/FLRS','REG']
-    ]
+if len(argv) >= 2:
+    folders = { argv[1] : folders.get(argv[1]) }
+
+#Sync Base
+ex = r"""
+                <Item>\2015\</Item>
+                <Item>\2016\</Item>
+                <Item>\2017\</Item>
+"""
+print "\nSyncing Base Folder"
+run_ffs(folders,exclude=ex) 
+
 for y in year:
     for m in range(1,13):
-    #for m in range(10, 13):
         print "\nSyncing %s %s" %(y, m)
-    
-        for f in folders:
-            drive  = f[0]
-            folder = f[1]
-            print "%s %s" %(strftime("%H:%M:%S %d/%m/%Y"),folder),
-
-            templ = open( 'flrs-tmp.tmpl' )
-            src = Template( templ.read() )
-            d={ 'drive':drive, 'folder':folder, 'y':y, 'm':m }
-            result = src.substitute(d)
-        
-            batch_file = "flrs"+folder+"_"+str(y)+str(m)+".ffs_batch"
-            ffsbatch = open( batch_file, 'w')
-            ffsbatch.write(result)
-            ffsbatch.close()
-
-            #raw_input("Press Enter to continue...")
-            ret, tr = 1, 1
-            while not ret == 0 and tr <= 3:
-                    ret = call(['C:\\Program Files\\FreeFileSync\\FreeFileSync.exe', batch_file])
-                    tr += 1 
-            if ret == 0:
-                print "<= Done"
-            elif ret == 1:
-                print "<= Done with Warnings!" , raw_input("\t Continue..?\n")
-            elif ret == 2:
-                print "<= Done with Errors!!"  , raw_input("\t Continue..?\n")
-            elif ret == 3:
-                print "<= Aborted!!!      "    , raw_input("\t Continue..?\n")
-            
-            remove(batch_file)
-
-
+        run_ffs(folders,y,m)
