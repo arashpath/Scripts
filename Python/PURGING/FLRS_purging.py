d74bae4adbb1734f842209f2f02a135ef248c048
Prashaath R <arashpath@gmail.com>
2017-08-09 19:16:36 +0530

    Finally Purging!!!

--- Python/PURGING/FLRS_purging.py
+++ Python/PURGING/FLRS_purging.py
@@ -2,57 +2,79 @@
 
 import pyodbc
 import os
-from humanbytes import hb
+import pickle
+from purging  import hb, fs_check
+from progressbar import ProgressBar, Bar, Counter, Timer, RotatingMarker
+
 
 #DataBase Connection String
-mssql = pyodbc.connect(
-    r'DRIVER={SQL Server};'
-    r'SERVER=192.168.11.201;'
-    r'DATABASE=FLRS;'
-    r'UID=sa;PWD=SAflrs@123').cursor()   #LocalDB
+cnxn = pyodbc.connect(
+    r'DRIVER={SQL Server};DATABASE=FLRS;'
+    r'SERVER=192.168.11.201;UID=sa;PWD=SAflrs@123' #LocalDB
+    )
+mssql = cnxn.cursor()
 
-arcLoc = r'G:\PURGED_DOCS\FLRS'
+def insert(row, status):
+    values = list(row)
+    'INSERT QUERY FOR PURGING LOG'
+    insert_query ='''INSERT INTO [dbo].[Purging_log] (
+        [LicType],[REFID],[IssuedDate],[ExpireDate],[DOC],[TableName],
+        [Status]) VALUES (?,?,?,?,?,?,?) ;'''
+    values.append(status)
+    try:
+        mssql.execute(insert_query, values)
+    except pyodbc.Error as pyerr :
+        print "MSSQLError: "+str(pyerr)
+    finally:
+        mssql.commit()
 
-purgeFiles = [
-    ['CLS', r'F:\FSSAI-DOCS1\FLRS'],
-    ['SLS', r'E:\FSSAI-DOCS\FLRS'],
-    ['REG', r'F:\FSSAI-DOCS1\FLRS']   
-] 
-
-all_sum = 0
-for p in purgeFiles:
-    (doc_type, base_path) = p
-    #base_path = 'D:\FSSAI-DOCS\FLRS' #OverRide Actual base path for local testing
-    sql_query = "exec FLRS.dbo.PurgingDOC ?"
-    mssql.execute(sql_query, 'get'+doc_type)
-    rows = mssql.fetchall()
-
-    with open(doc_type+'_purging.log', 'w') as log_file:
-        doc_sum = 0
-        for row in rows:
-            arcFile = os.path.normpath(row.PATH)
+def do_purg( doc_type, base_path, rows, arcLoc = r'G:\PURGED_DOCS\FLRS' ):
+    print "Purging "+doc_type
+    doc_sum, row_count = 0, 0
+    widgets = [Counter(), Bar(marker=RotatingMarker()), Timer()]
+    pbar = ProgressBar(widgets=widgets)
+    for row in pbar(rows):
+        if not fs_check(5,'G'):
+            print "\n Space Full !!"
+            with open(doc_type+'_pending.pickle', 'wb') as pending_rows:
+                pickle.dump( rows[row_count:]  ,pending_rows)
+            return doc_sum
+        else:
+            arcFile = os.path.normpath(row.DOC)
             srcPath = os.path.join(base_path, doc_type, arcFile)
             arcPath = os.path.join(arcLoc, doc_type, arcFile)
-            #print ("Moving from: "+srcPath+"\n\t to: "+arcPath)
-
             try:
                 if os.path.isfile(srcPath):
-                    f = os.path.getsize(srcPath)
-                    #os.renames(srcPath,arcPath)
-                    #print ("Archived: %s "+arcFile)
-                    doc_sum = doc_sum + f
-                    print >>log_file, "%s Found\t: %s" % ( hb(doc_sum), srcPath ) 
+                    fsize = os.path.getsize(srcPath)
+                    os.renames(srcPath, arcPath)
+                    doc_sum += fsize
+                    status = "Moved"
                 else:
-                    print >>log_file, "File Not Found\t: %s" % (srcPath)
-            except UnicodeError:
-                print "Error While Logging %s" % (srcPath)
+                    status = "NotFound"
+            except IOError as ioerr:
+                print "FileError: "+str(ioerr)
+                status = "ERROR"
+            insert(row, status)
+            row_count += 1   
+    print 80*"-"
+    print "Total Space Cleared in %s = %s" % (doc_type,  hb(doc_sum))
+    print 80*"-"
+    return doc_sum
 
-        print >>log_file, 30*"="
-        print >>log_file, "Grand Total of %s  = %s" % (doc_type,  hb(doc_sum))
-        print >>log_file, 30*"="
+sql_sp = "exec FLRS.dbo.PurgingDOC "  
 
-    all_sum = all_sum + doc_sum
-    print "%s Will be cleared in %s " % (hb(doc_sum), doc_type)
-    #raw_input("Cont.")
-
-print "Total Space Saved After Purging : %s" % (hb(all_sum))
+print "Getting Expired Lic Count..."
+total_space_cleared = 0
+for each in mssql.execute(sql_sp+'getCount').fetchall():
+    pickle_file = each.LicType+'_pending.pickle'
+    print "{} Total Lic: {} Expired Lic: {}".format(each.LicType, each.Total, 
+                                                    each.Expired)
+    if os.path.isfile(os.path.join(os.getcwd(), pickle_file)):
+        rows = pickle.load( open(pickle_file, 'rb') )
+    else:
+        rows = mssql.execute(sql_sp+"get"+each.LicType).fetchall()
+    space_cleared = do_purg( each.LicType, each.BasePath, rows )
+cnxn.close()
+print 80*"="
+print "Total Space Cleared : {} ".format(hb(total_space_cleared))
+print 80*"="
--- Python/PURGING/get0Bfiles.py
+++ Python/PURGING/get0Bfiles.py
@@ -1,7 +1,9 @@
 #!/bin/env python
 import sys
 import os
+import zipfile
 import progressbar
+
 
 if len(sys.argv) < 2:
     sys.exit('Usage getZeroKBfiles.exe "full:/path/of/folder/to/scan"')  
@@ -9,10 +11,15 @@ else:
     scan_folder = os.path.normpath(sys.argv[1])
 
 print 'Scanning '+scan_folder
-with open('ZeroBite.log', 'w') as zlog:
-    pbar = progressbar.ProgressBar()       
-    for contents in pbar(os.listdir(scan_folder)):
-        scan_file = os.path.join(scan_folder, contents)
-        if os.path.isfile(scan_file) and os.path.getsize(scan_file) == 0:
-            print >>zlog, scan_file
-            os.renames(scan_file, os.path.join(os.getcwd(), 'ZeroBite', contents))
+with zipfile.ZipFile('ZeroBite.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+    with open('ZeroBite.log', 'w') as zlog:
+        pbar = progressbar.ProgressBar()
+        for contents in pbar(os.listdir(scan_folder)):
+            scan_file = os.path.join(scan_folder, contents)
+            if os.path.isfile(scan_file) and os.path.getsize(scan_file) == 0:
+                zipf.write(scan_file, contents)
+                print >>zlog, scan_file
+                os.remove(scan_file)
+    zipf.write('ZeroBite.log')
+os.remove('ZeroBite.log')
+
