baa3eb8f64c27745f6507f2139a2d95809310f0e
Prashaath R <arashpath@gmail.com>
2017-09-13 17:33:59 +0530

    Non Intractive Moodle Setup Done

---
+++ Linux/fssai/Linux_Setup/final/moodle.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sed -e 
+echo "Installing Moodle"
+cd /opt/final
+#wget "https://download.moodle.org/stable33/moodle-3.3.1.tgz"
+tar -xzf moodle-3.3.1.tgz -C /opt/APPS/
+ln -s /opt/APPS/moodle /opt/apache/htdocs/moodle
+mkdir -p /opt/docs/moodledata
+chown daemon.daemon /opt/docs/moodledata /opt/APPS/moodle
+psql -c "CREATE ROLE moodleuser LOGIN ENCRYPTED PASSWORD 'moodle@123' NOINHERIT VALID UNTIL 'infinity';"
+psql -c "CREATE DATABASE moodledb WITH ENCODING='UTF8' OWNER moodleuser;"
+
+sudo -u daemon /opt/DevEnv/PHP/bin/php /opt/APPS/moodle/admin/cli/install.php --lang=en \
+--wwwroot=http://192.168.11.219/moodle \
+--dataroot=/opt/docs/moodledata \
+--dbtype='pgsql' \
+--dbhost='localhost' \
+--dbname='moodledb' \
+--dbuser='moodleuser' \
+--dbpass='moodle@123' \
+--fullname='NewMoodleSite' \
+--shortname='NewSite' \
+--adminuser='fssai' \
+--adminpass='F$$@i123' \
+--adminemail='fssai.cdc@gmail.com' \
+--agree-license \
+--non-interactive
--- Linux/fssai/Linux_Setup/final/postgreSQL.sh
+++ Linux/fssai/Linux_Setup/final/postgreSQL.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -e
 datadir='/opt/psqlDATA'
 #yum -y install "http://oscg-downloads.s3.amazonaws.com/packages/postgresql-9.5.6-1-x64-bigsql.rpm"
 yum -y localinstall postgresql-9.5.6-1-x64-bigsql.rpm
@@ -5,7 +7,7 @@ yum -y localinstall postgresql-9.5.6-1-x64-bigsql.rpm
 echo "source /opt/postgresql/pg95/pg95.env" >> ~/.bash_profile
 ln -s /opt/postgresql/pg95/pg95.env /etc/profile.d/pg95.sh
 
-#systemctl enable postgresql95 
-#systemctl start postgresql95
-#source /opt/postgresql/pg95/pg95.env
+systemctl enable postgresql95 
+systemctl start postgresql95
+source /opt/postgresql/pg95/pg95.env
 #psql 
