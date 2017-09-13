3c9847cfb6c38b861e19d93d2236c658430ce545
Prashaath R <arashpath@gmail.com>
2017-09-13 17:33:59 +0530

    Non Intractive Moodle Setup Done

--- Linux/fssai/Linux_Setup/final/php.sh
+++ Linux/fssai/Linux_Setup/final/php.sh
@@ -1,11 +1,13 @@
-cd /opt/final
+#!/bin/bash
+set -e
+#Install PHP
+echo "Installing PHP"
 #wget "http://in1.php.net/distributions/php-5.6.31.tar.gz"
 tar -xzf php-5.6.31.tar.gz 
 cd php-5.6.31/
 
 yum -y install libxml2-devel libcurl-devel libjpeg-turbo-devel libpng-devel freetype-devel libicu-devel gcc-c++ openldap-devel libxslt-devel
-./configure \
-  --prefix=/opt/DevEnv/PHP \
+./configure --prefix=/opt/DevEnv/PHP \
   --with-apxs2=/opt/apache/bin/apxs \
   --enable-mbstring \
   --with-curl \
@@ -27,14 +29,13 @@ yum -y install libxml2-devel libcurl-devel libjpeg-turbo-devel libpng-devel free
   --enable-opcache \
   --with-xsl && make && make install
 
-  
-  
 
 cat << EOF >> /opt/apache/conf/httpd.conf
 <IfModule php5_module>
 AddType application/x-httpd-php .php .php3 .phtml
 AddType application/x-httpd-php-source .phps
 </IfModule>
+
 
 EOF
 
@@ -46,15 +47,3 @@ echo "<?php
 ?>" > /opt/apache/htdocs/php/index.php
 
 systemctl restart apache 
-
-#############################################
-
-cd /opt/final
-#wget "https://download.moodle.org/stable33/moodle-3.3.1.tgz"
-tar -xzf moodle-3.3.1.tgz -C /opt/APPS/
-ln -s /opt/APPS/moodle /opt/apache/htdocs/moodle
-mkdir -p /opt/docs/moodledata
-chown daemon.daemon /opt/docs/moodledata /opt/APPS/moodle
-psql -c "CREATE ROLE moodleuser LOGIN ENCRYPTED PASSWORD 'moodle@123' NOINHERIT VALID UNTIL 'infinity';"
-psql -c "CREATE DATABASE moodledb WITH ENCODING='UTF8' OWNER moodleuser;"
-
---
+++ Linux/fssai/Linux_Setup/final/tomcat.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Installation DEVENV -------------------------------------------------------#
+set -e
+PKGS=$(dirname $(readlink -f "$0") )
+DEVENV=/opt/DevEnv
+mkdir $DEVENV
+# ---------------------------------------------------------------------------# 
+tomcatURL="http://archive.apache.org/dist/tomcat/tomcat-8/v8.5.9/bin/apache-tomcat-8.5.9.tar.gz"
+# Installing Tomcat ---------------------------------------------------------#
+echo -e "\nInstalling Tomcat\n"
+##wget $tomcatURL
+tar -xzf $PKGS/apache-tomcat-8*.tar.gz -C $DEVENV
+mv $DEVENV/apache-tomcat-8*/ $DEVENV/tomcat8-HOME
+HOME=$DEVENV/tomcat8-HOME
+BASE=/opt/APPS/default-tom0
+
+mkdir -p $BASE/{bin,conf,logs,work,webapps,temp}
+cp $HOME/conf/{server.xml,web.xml} $BASE/conf/
+
+cat <<EOF > $BASE/bin/server.sh
+export CATALINA_HOME=$HOME
+export CATALINA_BASE=$BASE
+export JAVA_HOME=$JAVA
+\$CATALINA_HOME/bin/catalina.sh \$@
+EOF
+
+chmod +x $BASE/bin/server.sh
+
+# Tomcat SystemCtl Script ----------------------#
+cat <<EOF > /etc/systemd/system/tomcat0.service
+# Systemd unit file for tomcat0 8080 port
+[Unit]
+Description=Apache Tomcat Web Application Container
+After=syslog.target network.target
+
+[Service]
+Type=forking
+
+Environment=CATALINA_HOME=$HOME
+Environment=CATALINA_BASE=$BASE
+Environment=CATALINA_PID=$BASE/temp/tomcat.pid
+
+ExecStart=/bin/sh -c '$BASE/bin/server.sh start'
+ExecStop=/bin/sh -c '$BASE/bin/server.sh stop'
+
+RestartSec=30
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo -e "\nTomcat Installation Completed\n"
+$BASE/bin/server.sh version
