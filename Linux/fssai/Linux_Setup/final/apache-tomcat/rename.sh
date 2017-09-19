a8887d93828b239c82cd4a729b47081c3457834f
Prashaath R <arashpath@gmail.com>
2017-09-19 09:23:37 +0530

    Rename.sh Corrected

--- Linux/fssai/Linux_Setup/final/apache-tomcat/rename.sh
+++ Linux/fssai/Linux_Setup/final/apache-tomcat/rename.sh
@@ -2,6 +2,6 @@
 ID=$1
 from=$(ls -lrth /opt/APPS/ | awk "/tom"$ID"/ "'{gsub(/.....$/,"");print $9}')
 to=$2
-mv -v /opt/APPS/{$from,$to}-tom0
-sed -i "s/$from-tom$ID/$to-tom$ID/" /etc/systemd/system/tomcat0.service /opt/APPS/$to-tom0/bin/server.sh
+mv -v /opt/APPS/{$from,$to}-tom$ID
+sed -i "s/$from-tom$ID/$to-tom$ID/" /etc/systemd/system/tomcat"$ID".service /opt/APPS/$to-tom$ID/bin/server.sh
 systemctl daemon-reload
