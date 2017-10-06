6b2f80068c276f1ab15514cf0de0fc9a407f9240
Prashaath R <arashpath@gmail.com>
2017-10-06 16:50:10 +0530

    LDAP Final

--- Linux/fssai/Linux_Setup/final/ldap.sh
+++ Linux/fssai/Linux_Setup/final/ldap.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Getting Domain Details
+svr='snfportal' ; com='in'
+echo "Enter Desired Domain:"
+#echo "DomineName (dc=svr)?:"; read $svr
+#echo "TLD Domain (dc=com)?:"; read $com
+
 #1 Install OpenLDAP Server. ================================================= # 
 echo "=========================== Install OpenLDAP Server\
  ==========================="
@@ -23,7 +30,7 @@ olcRootPW:
 EOF
 
 echo "Enter Admin Passwd :"
-sed -i "s/^olcRootPW:.*$/olcRootPW: $(slappasswd)/" chrootpw.ldif
+sed -i "s,^olcRootPW:.*$,olcRootPW: $(slappasswd)," chrootpw.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f chrootpw.ldif
 
 #3 Import basic Schemas. ==================================================== #
@@ -44,17 +51,17 @@ dn: olcDatabase={1}monitor,cn=config
 changetype: modify
 replace: olcAccess
 olcAccess: {0}to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth"
-  read by dn.base="cn=Manager,dc=srv,dc=world" read by * none
+  read by dn.base="cn=Manager,dc=fssai,dc=world" read by * none
 
 dn: olcDatabase={2}hdb,cn=config
 changetype: modify
 replace: olcSuffix
-olcSuffix: dc=srv,dc=world
+olcSuffix: dc=fssai,dc=world
 
 dn: olcDatabase={2}hdb,cn=config
 changetype: modify
 replace: olcRootDN
-olcRootDN: cn=Manager,dc=srv,dc=world
+olcRootDN: cn=Manager,dc=fssai,dc=world
 
 dn: olcDatabase={2}hdb,cn=config
 changetype: modify
@@ -65,45 +72,44 @@ dn: olcDatabase={2}hdb,cn=config
 changetype: modify
 add: olcAccess
 olcAccess: {0}to attrs=userPassword,shadowLastChange by
-  dn="cn=Manager,dc=srv,dc=world" write by anonymous auth by self write by * none
+  dn="cn=Manager,dc=fssai,dc=world" write by anonymous auth by self write by * none
 olcAccess: {1}to dn.base="" by * read
-olcAccess: {2}to * by dn="cn=Manager,dc=srv,dc=world" write by * read
+olcAccess: {2}to * by dn="cn=Manager,dc=fssai,dc=world" write by * read
 
 EOF
 
-echo "Enter Desired Domain:"
-echo "DomineName (dc=svr)?:"; read $svr
-echo "TLD Domain (dc=com)?:"; read $com
-sed -i "s/dc=srv,dc=world/dc=$svr,dc=$com/" chdomain.ldif
+sed -i "s/dc=fssai,dc=world/dc=$svr,dc=$com/" chdomain.ldif
 echo "Enter Domine Manager Passwd :"
-sed -i "s/^olcRootPW:.*$/olcRootPW: $(slappasswd)/" chdomain.ldif
+sed -i "s,^olcRootPW:.*$,olcRootPW: $(slappasswd)," chdomain.ldif
 ldapmodify -Y EXTERNAL -H ldapi:/// -f chdomain.ldif 
 
 
 cat << EOF > basedomain.ldif
 # replace to your own domain name for "dc=***,dc=***" section
 
-dn: dc=srv,dc=world
+dn: dc=fssai,dc=world
 objectClass: top
 objectClass: dcObject
 objectclass: organization
-o: Server World
-dc: Srv
+o: fssai world
+dc: fssai
 
-dn: cn=Manager,dc=srv,dc=world
+dn: cn=Manager,dc=fssai,dc=world
 objectClass: organizationalRole
 cn: Manager
 description: Directory Manager
 
-dn: ou=People,dc=srv,dc=world
+dn: ou=People,dc=fssai,dc=world
 objectClass: organizationalUnit
 ou: People
 
-dn: ou=Group,dc=srv,dc=world
+dn: ou=Group,dc=fssai,dc=world
 objectClass: organizationalUnit
 ou: Group
 
 EOF
 
-sed -i "s/dc=srv,dc=world/dc=$svr,dc=$com/" basedomain.ldif
+sed -i "s/dc=fssai,dc=world/dc=$svr,dc=$com/
+        s/^o: fssai world$/o: $svr $com/
+        s/^dc: fssai$/dc: $svr/" basedomain.ldif
 ldapadd -x -D cn=Manager,dc=$svr,dc=$com -W -f basedomain.ldif 
