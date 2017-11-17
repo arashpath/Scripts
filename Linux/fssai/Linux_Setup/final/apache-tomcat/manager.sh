762fce299808fc0081ef8369f00de90063565c4c
Prashaath R <arashpath@gmail.com>
2017-11-17 15:13:56 +0530

    fixes in Manager

--- Linux/fssai/Linux_Setup/final/apache-tomcat/manager.sh
+++ Linux/fssai/Linux_Setup/final/apache-tomcat/manager.sh
@@ -4,9 +4,12 @@ ID=$1
 DEVENV=/opt/DevEnv ; HOME=$DEVENV/tomcat8-HOME
 BASE=/opt/APPS/$(ls -lrth /opt/APPS/ | awk "/-tom"$ID"/ "'{print $9}')
 # ---------------------------------------------------------------------------# 
-[ ! -d "$BASE" ] && echo "Directory $BASE DOES NOT exists." && exit 
-cp -a $HOME/webapps/manager $BASE/webapps/
-sed -i '/<Context/a <!-- 
-	/<\/Context/i -->' $BASE/webapps/manager/META-INF/context.xml
-cp -a $HOME/conf/tomcat-users.* $BASE/conf/
-sed -i '/<\/tomcat-users>/i <user username="admin" password="password" roles="manager-gui,admin-gui"/>' tomcat-users.xml
+if [ ! -d "$BASE" ] 
+then 
+  cp -a $HOME/webapps/manager $BASE/webapps/
+  sed -i '/<Context/a <!--
+        /<\/Context/i -->' $BASE/webapps/manager/META-INF/context.xml
+  cp -a $HOME/conf/tomcat-users.* $BASE/conf/
+  sed -i '/<\/tomcat-users>/i <user username="Fadmin" password="3Cat" roles="manager-gui,admin-gui"/>' $BASE/conf/tomcat-users.xml
+  systemctl restart tomcat$ID
+fi
