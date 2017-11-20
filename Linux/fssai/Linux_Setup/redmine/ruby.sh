7535370457eb3970b24f0a32d9cd407ca93a6912
Prashaath R <arashpath@gmail.com>
2017-11-20 15:26:51 +0530

    Ruby Setup

---
+++ Linux/fssai/Linux_Setup/redmine/ruby.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Installation DEVENV
+set -e 
+PKGS=$(dirname $(readlink -f "$0") )
+DEVENV=/opt/DevEnv
+mkdir -p $DEVENV
+
+# Ruby URL
+rubyURL="https://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.2.tar.gz"
+rubySUM="93b9e75e00b262bc4def6b26b7ae8717efc252c47154abb7392e54357e6c8c9c"
+
+# Installing Ruby ------------------------------------------------------------#
+cd $PKGS; curl --remote-name --progress $rubyURL
+echo "$rubySUM $(ls ruby-*.tar.gz)" \
+ | sha256sum -c - && tar xzf ruby-*.tar.gz
+cd ruby-*/ && ./configure --prefix=$DEVENV/ruby/ && make && make install
+
+echo "Ruby Installed ..."
+$DEVENV/ruby/bin/ruby -v
