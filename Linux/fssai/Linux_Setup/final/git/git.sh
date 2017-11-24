953746e419ad27a7c2ce8da778a825c73b2c1d4f
Prashaath R <arashpath@gmail.com>
2017-11-24 12:29:23 +0530

    Changes In Setup

---
+++ Linux/fssai/Linux_Setup/final/git/git.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Installation DEVENV
+set -e 
+PKGS=$(dirname $(readlink -f "$0") )
+DEVENV=/opt/DevEnv
+mkdir -p $DEVENV
+
+# Git URL 
+ gitURL="https://www.kernel.org/pub/software/scm/git/git-2.15.0.tar.gz"
+ gitSUM="25762cc50103a6a0665c46ea33ceb0578eee01c19b6a08fd393e8608ccbdb3da" 
+
+# Installing GIT ------------------------------------------------------------#
+yum -y install make automake gcc gcc-c++ kernel-devel zlib-devel \
+ libyaml-devel openssl-devel gdbm-devel pcre2-devel readline-devel \
+ ncurses-devel libffi-devel curl openssh-server libxml2-devel libxslt-devel \
+ libcurl-devel libicu-devel logrotate python-docutils pkgconfig cmake
+
+yum install perl-CPAN
+cpan -i ExtUtils::MakeMaker
+
+cd $PKGS; curl --remote-name --progress $gitURL
+echo "$gitSUM $(ls  git-*.tar.gz)" \
+ | sha256sum -c - && tar xzf git-*.tar.gz 
+cd git-* && ./configure --prefix=$DEVENV/git/ && make all && make install
+
+echo "Git Installed ..."
+$DEVENV/git/bin/git --version
--- Linux/fssai/Linux_Setup/final/setup.sh
+++ Linux/fssai/Linux_Setup/final/setup.sh
@@ -3,12 +3,23 @@
 set -e 
 PKGS=$(dirname $(readlink -f "$0") )
 DEVENV=/opt/DevEnv
-mkdir $DEVENV
+mkdir -p $DEVENV
+
+ln -s /opt/DevEnv/dev.env /etc/profile.d/devEnv.sh
 
 #Installing Apache HTTPD
-echo "Installing Apache ..." && sleep 5 && sh $PKGS/apache-httpd/httpd.sh
+echo "Installing Apache ..."     && sleep 5 && sh $PKGS/apache-httpd/httpd.sh
 
 #Installing Tomcat & JAVA
-echo "Installing Tomcat ..." && sleep 5 && sh $PKGS/apache-tomcat/tomcat.sh
+echo "Installing Tomcat ..."     && sleep 5 && sh $PKGS/apache-tomcat/tomcat.sh
 
-echo "Installing Tomcat ..." && sleep 5 && sh $PKGS/apache-tomcat/modjk.sh
+echo "Installing Modjk ..."      && sleep 5 && sh $PKGS/apache-tomcat/modjk.sh
+
+#Installing PostgreSQL
+echo "Installing PostgreSQL ..." && sleep 5 && sh $PKGS/postgres/postgreSQL.sh
+
+#Installing Ruby
+echo "Installing Ruby ..."       && sleep 5 && sh $PKGS/ruby/ruby.sh
+
+#Installing Git
+echo "Installing Git ..."        && sleep 5 && sh $PKGS/git/git.sh
