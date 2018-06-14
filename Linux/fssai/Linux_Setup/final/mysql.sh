565742abebecaa5970f9e5e783477c242c1644ff
Prashaath R <fssai.cdc@gmail.com>
2018-06-14 17:52:20 +0530

    mysql install working

--- Linux/fssai/Linux_Setup/final/mysql.sh
+++ Linux/fssai/Linux_Setup/final/mysql.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-MyPass="fss@1"
+MyPass="MyPASS"
 
 yum install -y mariadb-server
 mv /var/lib/mysql /opt/mysqlDATA
 sed -i 's/\/var\/lib\/mysql/\/opt\/mysqlDATA/p' /etc/my.cnf
 systemctl start mariadb
+
+echo "[client]
+socket=/opt/mysqlDATA/mysql.sock" >  ~/.my.cnf
 
 mysql -u root <<-EOF
 UPDATE mysql.user SET Password=PASSWORD('$MyPass') WHERE User='root';
@@ -17,6 +20,5 @@ DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEGES;
 EOF
 
-echo "[client]
-user=root
-password=$MyPass" >  ~/.my.cnf
+echo "user=root
+password=$MyPass" >>  ~/.my.cnf
